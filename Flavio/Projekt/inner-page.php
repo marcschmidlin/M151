ba256f1533dca7be48a7d6b7cf9296b66a234db1
marcschmidlin <marc.schmidlin@baloise.ch>
2023-04-18 23:15:38 +0200

    adjust details

--- Flavio/Projekt/inner-page.php
+++ Flavio/Projekt/inner-page.php
@@ -106,40 +106,19 @@ $result->free();
   <header id="header" class="fixed-top header-inner-pages">
     <div class="container d-flex align-items-center justify-content-lg-between">
 
-      <h1 class="logo me-auto me-lg-0"><a href="index.html">Gp<span>.</span></a></h1>
+      <h1 class="logo me-auto me-lg-0"><a href="./index.php">FM Fitness<span>.</span></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
-          <li><a class="nav-link scrollto " href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto " href="./index.php">Home</a></li>
           <li><a class="nav-link scrollto" href="#about">About</a></li>
           <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-        </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
 
-      <a href="#about" class="get-started-btn scrollto">Get Started</a>
+      <a href="#about" class="get-started-btn scrollto">Logged In <?php echo $vorname?></a>
 
     </div>
   </header><!-- End Header -->
@@ -151,10 +130,10 @@ $result->free();
       <div class="container">
 
         <div class="d-flex justify-content-between align-items-center">
-          <h2>Inner Page</h2>
+          <h2>Übersicht</h2>
           <ol>
-            <li><a href="index.html">Home</a></li>
-            <li>Inner Page</li>
+            <li><a href="./index.php">Home</a></li>
+            <li>Übersicht</li>
           </ol>
         </div>
 
@@ -175,7 +154,7 @@ $result->free();
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
-          <h2>Funktionen Überischt</h2>
+          <h2>Funktionen Übersicht</h2>
           <p>Was willst du als nächstes machen?</p>
         </div>
 
@@ -195,7 +174,7 @@ $result->free();
           <a href="./trainingsplan-erstellen.php">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-file"></i></div>
-              <h4><a href="./traingsplan-erstellen.php">Trainingsplan erstellen</a></h4>
+              <h4><a href="./trainingsplan-erstellen.php">Trainingsplan erstellen</a></h4>
               <p>Erstelle dir einen neuen Trainingsplan oder füge einen aus der Gallerie hinzu.</p>
             </div>
             </a>
